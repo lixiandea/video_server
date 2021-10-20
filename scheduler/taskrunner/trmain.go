d6066c59e8379988417df38252903b2bf21e14d2
dijiang <dijiang@bytedance.com>
2021-10-20 23:44:28 +0800

    + add worker

--- scheduler/taskrunner/trmain.go
+++ scheduler/taskrunner/trmain.go
@@ -1,1 +1,31 @@
 package taskrunner
+
+import "time"
+
+type Worker struct {
+	ticker *time.Ticker
+	runner *Runner
+}
+
+func NewWorker(inteval time.Duration, r *Runner) *Worker {
+	return &Worker{
+		ticker: time.NewTicker(inteval * time.Second),
+		runner: r,
+	}
+}
+
+func (w *Worker) startWorker() {
+	for {
+		select {
+		case <-w.ticker.C:
+			go w.runner.startAll()
+		}
+	}
+}
+
+func Start() {
+	r := NewRunner(30, false, VideoClearDispatcher, VideoClearExecutor)
+	w := NewWorker(30, r)
+	w.startWorker()
+
+}
